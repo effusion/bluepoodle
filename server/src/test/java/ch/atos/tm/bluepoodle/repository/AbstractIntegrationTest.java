98a3a849ac0dcfb14521fa1f3b8742c2f5e40d9f
a539428 <a539428@bdea7c0b-7e81-49a6-8353-b99fe96bb420>
2014-02-21 08:04:40 +0000

    Removed populate methods because no longer needed.
    git-svn-id: https://astra3.ait-ch.ch/tm-bluepoodle/trunk@43 bdea7c0b-7e81-49a6-8353-b99fe96bb420

--- server/src/test/java/ch/atos/tm/bluepoodle/repository/AbstractIntegrationTest.java
+++ server/src/test/java/ch/atos/tm/bluepoodle/repository/AbstractIntegrationTest.java
@@ -1,57 +1,9 @@
 package ch.atos.tm.bluepoodle.repository;
 
-import java.sql.Connection;
-import java.sql.SQLException;
-
-import javax.sql.DataSource;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.jdbc.datasource.DataSourceUtils;
-import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
 import org.springframework.transaction.annotation.Transactional;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
 
 @ContextConfiguration(locations={"classpath:application-context-test.xml"})
 @Transactional
-public abstract class AbstractIntegrationTest extends AbstractTestNGSpringContextTests{
-
-	@Autowired
-	DataSource dataSource;
-
-	/**
-	 * Populates the configured {@link DataSource} with data from {@code data.sql}.
-	 * 
-	 * @throws SQLException
-	 */
-	@BeforeClass
-	public void populateDatabase() throws SQLException {
-		/*ResourceDatabasePopulator populator = new ResourceDatabasePopulator();
-		populator.addScript(new ClassPathResource("data.sql"));
-		executePopulator(populator);*/
-	}
-
-	private void executePopulator(ResourceDatabasePopulator populator)
-			throws SQLException {
-		Connection connection = null;
-		try {
-			connection = DataSourceUtils.getConnection(dataSource);
-			populator.populate(connection);
-		} finally {
-			if (connection != null) {
-				DataSourceUtils.releaseConnection(connection, dataSource);
-			}
-		}
-	}
-	@AfterClass
-	public void deleteTables() throws SQLException{
-		/*ResourceDatabasePopulator populator = new ResourceDatabasePopulator();
-		populator.setContinueOnError(false);
-		populator.setIgnoreFailedDrops(false);
-		populator.addScript(new ClassPathResource("droptables.sql"));
-		executePopulator(populator);*/
-	}
-}
+public abstract class AbstractIntegrationTest extends AbstractTestNGSpringContextTests{}
