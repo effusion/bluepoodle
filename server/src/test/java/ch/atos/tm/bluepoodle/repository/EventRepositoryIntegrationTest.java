e1092bcb0ac8b7174d97c96a51bc967379b3930d
a539428 <a539428@bdea7c0b-7e81-49a6-8353-b99fe96bb420>
2014-03-20 05:53:59 +0000

    Corrected test
    git-svn-id: https://astra3.ait-ch.ch/tm-bluepoodle/trunk@64 bdea7c0b-7e81-49a6-8353-b99fe96bb420

--- server/src/test/java/ch/atos/tm/bluepoodle/repository/EventRepositoryIntegrationTest.java
+++ server/src/test/java/ch/atos/tm/bluepoodle/repository/EventRepositoryIntegrationTest.java
@@ -47,6 +47,6 @@ public class EventRepositoryIntegrationTest extends AbstractIntegrationTest {
 		BooleanExpression isEventType = qEvent.eventType.name.eq("Entwicklerkonferenz");
 		Iterable<Event> result = eventRepository.findAll(isEventType);
 		List<Event> events = Lists.newArrayList(result);
-		assertEquals(2, events.size());
+		assertEquals(3, events.size());
 	}
 }
